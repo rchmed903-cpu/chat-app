36c32dc09371f990163de2ab04dc8da4b36a62f7
rchmed903 <rchmed903@gmail.com>
2026-05-09 13:17:21 +0100

    remove node_modules

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ const server = http.createServer(app);
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://chat-app-client.vercel.app",
+  "https://chat-app-git-main-rchmed903-cpus-projects.vercel.app",
+  "https://chat-3iu1hiqdi-rchmed903-cpus-projects.vercel.app",
 ];
 
 const io = new Server(server, {
